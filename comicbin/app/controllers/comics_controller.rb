0e527022dfa7491649fb2cdbcb7abf4691984970
Dan Stankiewicz <daniel.stankiewicz@gmail.com>
2014-02-10 19:33:28 -0600

    Homework 5
    All functionality in place.   Must do a rake db:migrate and db:seed to
    initialize the app.
    
    Both localhost:3000/ and localhost:3000/comics will show the index page.

--- comicbin/app/controllers/comics_controller.rb
+++ comicbin/app/controllers/comics_controller.rb
@@ -1,7 +1,8 @@
 class ComicsController < ApplicationController
 
 	def index
-		@comics = Comic.all
+    #order comics so that newly added ones are first
+		@comics = Comic.all.order("id desc")
 		render 'index'
 	end
 
@@ -11,28 +12,28 @@ class ComicsController < ApplicationController
 	end
 
 	def show
-    	comic_id = params[:id]
-    	@comic = Comic.find_by :id => comic_id
-    	render 'show'
-  	end
+   	comic_id = params[:id]
+   	@comic = Comic.find_by :id => comic_id
+   	render 'show'
+  end
 
-  	def create
-  		@comic = Comic.new
-  		@comic.title = params[:title]
-  		@comic.description = params[:description]
-  		@comic.image_url = params[:image_url]
-  		@comic.save
+  def create
+  	@comic = Comic.new
+  	@comic.title = params[:title]
+  	@comic.description = params[:description]
+  	@comic.image_url = params[:image_url]
+  	@comic.save
 
-  		redirect_to '/comics/'+@comic.id.to_s
-  	end
+  	redirect_to '/comics/'+@comic.id.to_s
+  end
 
-  	def delete
-    	comic_id = params[:id]
-    	@comic = Comic.find_by :id => comic_id
-  		@comic.destroy
+  def delete
+   	comic_id = params[:id]
+   	@comic = Comic.find_by :id => comic_id
+  	@comic.destroy
 
-  		redirect_to '/comics'
-  	end
+  	redirect_to '/comics'
+  end
 
 
 end
